68f6cd5cbab39d22aaf2d6637a06fa7c5a4a526a
sebastian705 <sebastian705@users.noreply.github.com>
2026-05-06 00:25:28 -0500

    feat(user): implementar funciones CRUD

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = User::get(['id', 'name']);
+        return response()->json($data, 200);
+    }
+
+    public function show($id)
+    {
+        $data = User::find($id);
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //validacion
+
+        $data = User::find($id);
+        $data->fill($request->all());
+        $data->save();
+        return response()->json($data, 200);
+
+    }
 }
