2e9c62648eec2d90aec49f3c25220123664c4f8d
sebastian705 <sebastian705@users.noreply.github.com>
2026-05-06 00:39:13 -0500

    fix(api): signo de auth

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::prefix('v1')->group(function () {
     Route::post('/auth/login', [AuthController::class, 'login']);
 
     ///Private
-    Route::group(['middleware'=> 'auth/sanctum'], function () {
+    Route::group(['middleware' => 'auth:sanctum'], function () {
         //::auth
         Route::post('/auth/logout', [AuthController::class, 'logout']);
         //::rol client
