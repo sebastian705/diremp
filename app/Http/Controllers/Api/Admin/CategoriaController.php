704c1df2e5901c5ce6ce62d101d093561d60177f
sebastian705 <sebastian705@users.noreply.github.com>
2026-05-06 00:22:46 -0500

    feat(categoria): implementar CRUD completo

--- app/Http/Controllers/Api/Admin/CategoriaController.php
+++ app/Http/Controllers/Api/Admin/CategoriaController.php
@@ -3,9 +3,58 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Categoria::orderBy("orden")->get(["id", "name"]);
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        //validacion
+        $data = new Categoria($request->all());
+        //upload image base64
+        if ($request->urlfoto) {
+            $img = $request->urlfoto;
+            //process
+            $folderPath = "/img/categoria/";
+            $image_parts = explode(";base64,", $img);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+
+            // 3. Decodificar el contenido Base64
+            $image_base64 = base64_decode($image_parts[1]);
+
+            // 4. Crear un nombre único para el archivo
+            $file = $folderPath . Str::slug($request->nombre) . '.' . $image_type;
+            file_put_contents(public_path($file), $image_base64);
+
+            $data->urlfoto = Str::slug($request->nombre) . '.' . $image_type;
+        }
+        $data->save();
+
+        return response()->json($data, 200);
+    }
+
+    public function show($id)
+    {
+        $data = Categoria::find($id);
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //validacion
+
+        $data = Categoria::find($id);
+        $data->fill($request->all());
+        $data->save();
+        return response()->json($data, 200);
+    }
 }
