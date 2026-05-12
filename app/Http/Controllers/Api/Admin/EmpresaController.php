13086ed6cbb3b7966a14829dcb0867f6cfb7f4b0
sebastian705 <sebastian705@users.noreply.github.com>
2026-05-12 14:54:44 -0500

    feat(empresa): implementar funciones CRUD

--- app/Http/Controllers/Api/Admin/EmpresaController.php
+++ app/Http/Controllers/Api/Admin/EmpresaController.php
@@ -3,9 +3,82 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Empresa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class EmpresaController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Empresa::orderBy("orden")->get(["id", "nombre"]);
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        //validacion
+        $data = new Empresa($request->all());
+        //upload image base64
+        if ($request->urlfoto) {
+            $img = $request->urlfoto;
+            //process
+            $folderPath = "/img/categoria/";
+            $image_parts = explode(";base64,", $img);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+
+            // 3. Decodificar el contenido Base64
+            $image_base64 = base64_decode($image_parts[1]);
+
+            // 4. Crear un nombre único para el archivo
+            $file = $folderPath . Str::slug($request->nombre) . '.' . $image_type;
+            file_put_contents(public_path($file), $image_base64);
+
+            $data->urlfoto = Str::slug($request->nombre) . '.' . $image_type;
+        }
+        
+        $data->save();
+        return response()->json($data, 200);
+    }
+
+    public function show($id)
+    {
+        $data = Empresa::find($id);
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //validacion
+        $data = Empresa::find($id);
+        $data->fill($request->all());
+
+        if ($request->urlfoto) {
+            $img = $request->urlfoto;
+            //process
+            $folderPath = "/img/Empresa/";
+            $image_parts = explode(";base64,", $img);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+
+            // 3. Decodificar el contenido Base64
+            $image_base64 = base64_decode($image_parts[1]);
+
+            // 4. Crear un nombre único para el archivo
+            $file = $folderPath . Str::slug($request->nombre) . '.' . $image_type;
+            file_put_contents(public_path($file), $image_base64);
+
+            $data->urlfoto = Str::slug($request->nombre) . '.' . $image_type;
+        }
+        $data->save();
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id) 
+    {
+        $data = Empresa::find($id);
+        $data->delete();
+        return response()->json("Borrado", 200);
+    }
 }
